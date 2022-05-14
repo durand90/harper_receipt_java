419b7e813468c42747485f3fc64248bf63dc3593
Fanfan durand <durand90@live.com>
2022-05-14 15:15:43 -0400

    First commit

---
+++ src/main/java/com/fanfan/harperreceipt/HarperReceiptApplication.java
@@ -0,0 +1,13 @@
+package com.fanfan.harperreceipt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarperReceiptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarperReceiptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fanfan/harperreceipt/HarperReceiptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fanfan.harperreceipt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarperReceiptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
